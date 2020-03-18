ebf0ae6f6afb33689be23f54a72bef80c4facc45
Samuel <diogosamuelfernandes@outlook.com>
2020-03-18 19:38:08 +0000

    Modular Code

--- include/log.h
+++ include/log.h
@@ -2,8 +2,21 @@
 #define LOG_H_INCLUDED
 
 /**
- * @brief 
+ * @brief           Init the log
+ * @return          0 uppon sucess or 1 otherwhise
+ */
+int init_log();
+
+/**
+ * @brief           Write a line to log
+ * @return          0 uppon sucess or 1 otherwhise
  */
 int write_log();
+
+/**
+ * @brief           Close log file
+ * @return          0 uppon sucess or 1 otherwhise
+ */
+int close_log();
 
 #endif // LOG_H_INCLUDED
--- src/log.c
+++ src/log.c
@@ -8,21 +8,38 @@
 #include <utils.h>
 #include <errno.h>
 
-int write_log() {
-  if(getenv("LOG_FILENAME") != NULL) { // Write log to LOG_FILENAME
-      printf("Write log to LOG_FILENAME");
-  }
-  else { // Write log to a previous file
-      int fileLog = open("log.txt", O_WRONLY | O_EXCL | O_CREAT, 0644);
-      if (fileLog == -1) {
-          printf("%s", strerror(errno));
-          exit(1);
-      }
+int fileLog;
 
-      if (close(fileLog) != 0) {
-          printf("%s", strerror(errno));
-          exit(1);
-      }
-  }
-  return 0;
+int init_log() {
+    if(getenv("LOG_FILENAME") != NULL) { // Write log to LOG_FILENAME
+        fileLog = open("LOG_FILENAME" , O_WRONLY | O_CREAT, 0644);
+    }
+    else { // Write log to a pre defined file
+        fileLog = open("log.txt", O_WRONLY | O_CREAT, 0644);
+    }
+
+    if (fileLog == -1) { // Test if file is open
+        printf("%s\n", strerror(errno));
+        return 1;
+    }
+
+    // Write the first line of log
+    char str[] = "instant – pid – action – info\n";
+    write(fileLog, str, sizeof(str)/ sizeof(char));
+
+    return 0;
+}
+
+int write_log() {
+    // To do
+
+    return 0;
+}
+
+int close_log() {
+    if (close(fileLog) != 0) {
+        printf("%s", strerror(errno));
+        return 1;
+    }
+    return 0;
 }
