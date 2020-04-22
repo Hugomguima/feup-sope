28c33edc22e93ef1695d633fe17251c81d5a9df8
Samuel <diogosamuelfernandes@outlook.com>
2020-04-22 17:40:36 +0100

    Start doing things, but messy code

--- projeto-2/client/src/main.c
+++ projeto-2/client/src/main.c
@@ -1,14 +1,48 @@
-#include "parse.h"
-
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h>
-#include <string.h>
 #include <sys/unistd.h>
+#include <string.h>
+#include <pthread.h>
 
 #define BUFFER_SIZE 255
 
-int main(int argc, char *argv[]) {
+typedef struct {
+    double i; 
+    pid_t pid;
+    pthread_t tid;
+    int dur;
+    int pl;
+} request_t;
 
+void *thread_toilet(void *path) {
+    request_t req;
+    req.i = 2;
+
+    req.pid = getpid();
+
+    int req_fifo = open(path, O_WRONLY);
+    write(req_fifo, &req, sizeof(request_t));
+    close(req_fifo);
+
+    char ans_fifo_path[1024];
+    sprintf(ans_fifo_path, "/tmp/%d", req.pid);
+    mkfifo(ans_fifo_path, 0660);
+    int ans_fifo = open(ans_fifo_path, O_RDONLY);
+
+    double id;
+    read(ans_fifo, &id, sizeof(double));
+    close(ans_fifo);
+    unlink(ans_fifo_path);
+
+    printf("%lf\n", id);
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
     if (argc != 3) {
         char error[BUFFER_SIZE];
         char *s = strerror(EINVAL);
@@ -18,8 +52,13 @@ int main(int argc, char *argv[]) {
         write(STDERR_FILENO, error, strlen(error));
         return EINVAL;
     }
+    
+    char *path_name = argv[2];
 
-    printf("Flags: %x\n", parse_cmd(argc - 1, &argv[1]));
-
+    pthread_t tid;
+    for(int i = 0; i < atoi(argv[1]); i++) {
+        pthread_create(&tid, NULL, thread_toilet, path_name);
+        sleep(1);
+    }
     return 0;
 }
--- projeto-2/server/src/main.c
+++ projeto-2/server/src/main.c
@@ -1,11 +1,37 @@
 #include "parse.h"
 
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h>
-#include <string.h>
 #include <sys/unistd.h>
+#include <string.h>
+#include <pthread.h>
 
 #define BUFFER_SIZE 255
+
+typedef struct {
+    double i; 
+    pid_t pid;
+    pthread_t tid;
+    int dur;
+    int pl;
+} request_t;
+
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER; // mutex
+
+void *th_operation(void *arg) {
+    request_t *req = arg;
+    double id = req->i; 
+
+    char buf[1024];
+    sprintf(buf, "/tmp/%d", req->pid);
+    int ans_fifo = open(buf, O_WRONLY);
+    write(ans_fifo, &id, sizeof(double));
+    return NULL;
+}
 
 int main(int argc, char *argv[]) {
 
@@ -19,7 +45,19 @@ int main(int argc, char *argv[]) {
         return EINVAL;
     }
 
-    printf("Flags: %x\n", parse_cmd(argc - 1, &argv[1]));
+    char *path_name = argv[2];
 
+    mkfifo(path_name, 0660);
+    int req_fifo = open(path_name, O_RDONLY);
+
+    request_t *req = malloc(sizeof(request_t));
+    pthread_t tid;
+    int r;
+    while((r = read(req_fifo, req, sizeof(request_t))) != -1) {
+        pthread_create(&tid, NULL, th_operation, req);
+    }
+    free(req);
+    close(req_fifo);
     return 0;
 }
+
