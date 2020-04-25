fc55b2a158076dc980a499c81d8d5bba2021f913
Telmooo <telmoabaptista@hotmail.com>
2020-04-26 00:21:03 +0100

    working (on the server) maybe, prolly not tho

--- projeto-2/server/src/main.c
+++ projeto-2/server/src/main.c
@@ -2,10 +2,13 @@
 
 /* INCLUDE HEADERS */
 #include "parse.h"
+#include "protocol.h"
+#include "sync.h"
 #include "utils.h"
 
 /* SYSTEM CALLS HEADERS */
 #include <fcntl.h>
+#include <signal.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -19,28 +22,69 @@
 /* Miscellaneous */
 #include <pthread.h>
 
-#define BUFFER_SIZE 256
+#include <time.h>
 
-typedef struct {
-    int id;
-    pid_t pid;
-    pthread_t tid;
-    int dur;
-    int pl;
-} request_t;
+#define BUFFER_SIZE     256
+
+#define MAX_THREADS     500
 
 pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER; // mutex
 
-void *th_operation(void *arg){
-    pthread_mutex_lock(&mut);
-    request_t *req = arg;
-    printf("%d\n", req->id);
+int bath_open = 1;
 
-    char buf[1024];
-    sprintf(buf, "/tmp/%d.%ld", req->pid, req->tid);
-    int ans_fifo = open(buf, O_WRONLY);
-    write(ans_fifo, req, sizeof(request_t));
-    pthread_mutex_unlock(&mut);
+int order = 1;
+
+void sigalarm_handler() {
+    bath_open = 0;
+}
+
+void *th_operation(void *arg){
+
+    if (arg == NULL) {
+        return NULL;
+    }
+
+    request_t *request = (request_t*)arg;
+
+    char reply_fifo_path[BUFFER_SIZE];
+    sprintf(reply_fifo_path, "/tmp/%d.%ld", request->pid, request->tid);
+    int reply_fifo;
+    if ((reply_fifo = open(reply_fifo_path, O_WRONLY)) == -1) {
+        free(request);
+        return NULL;
+    }
+
+    sem_t *sem_reply;
+
+    if (sem_open_reply(sem_reply, request->pid, request->tid)) {
+        close(reply_fifo);
+        unlink(reply_fifo_path);
+        return NULL;
+    }
+
+    request_t reply;
+    fill_reply(&reply, request->id, request->pid, request->tid, request->dur, order++);
+
+    free(request);
+
+    if (write_reply(reply_fifo, &reply)) {
+        return NULL;
+    }
+
+    if (sem_post_reply(sem_reply)) {
+        close(reply_fifo);
+        unlink(reply_fifo_path);
+        return NULL;
+    }
+
+    if (close(reply_fifo)) {
+        return NULL;
+    }
+
+    if (unlink(reply_fifo_path)) {
+        return NULL;
+    }
+
     return NULL;
 }
 
@@ -66,21 +110,99 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    char req_fifo_path[256];
+    char req_fifo_path[BUFFER_SIZE];
     sprintf(req_fifo_path, "/tmp/%s", info.path);
 
-    mkfifo(req_fifo_path, 0660);
-    int req_fifo = open(req_fifo_path, O_RDONLY);
+    free_parse_info(&info);
 
-    request_t *req = malloc(sizeof(request_t));
-    pthread_t tid;
-    int r;
-    while((r = read(req_fifo, req, sizeof(request_t))) != -1){
-        if(r != sizeof(request_t)) continue;
-        pthread_create(&tid, NULL, th_operation, req);
+    if (mkfifo(req_fifo_path, 0660)) {
+        perror("make fifo");
+        return errno;
     }
+
+    int req_fifo;
+    if ((req_fifo = open(req_fifo_path, O_RDONLY | O_NONBLOCK)) == -1) {
+        unlink(req_fifo_path);
+        perror("open fifo");
+        return errno;
+    }
+
+    if (init_sync(O_CREAT)) {
+        close(req_fifo);
+        unlink(req_fifo_path);
+        perror("failed to init sync");
+        return errno;
+    }
+
+    struct sigaction alarm_action;
+    alarm_action.sa_handler = sigalarm_handler;
+    sigemptyset(&alarm_action.sa_mask);
+    alarm_action.sa_flags = 0;
+    sigaction(SIGALRM, &alarm_action, NULL);
+
+    pthread_t tid;
+
+    int thread_counter = 0;
+
+    request_t *request;
+
+    time_t initial = time(NULL);
+
+    alarm(info.exec_secs);
+
+    while (bath_open) {
+        if (sem_post_send_request()) {
+            free_sync();
+            close(req_fifo);
+            unlink(req_fifo_path);
+            return errno;
+        }
+
+        if (sem_wait_receive_request()) {
+            if (errno != EINTR) {
+                free_sync();
+                close(req_fifo);
+                unlink(req_fifo_path);
+                return errno;
+            }
+        }
+
+
+        if (bath_open) {
+            request = (request_t*)malloc(sizeof(request_t));
+
+            if (read_request(req_fifo, request)) {
+                free_sync();
+                close(req_fifo);
+                unlink(req_fifo_path);
+                free(request);
+                perror("read request");
+                return errno;
+            }
+            printf("received request");
+            if (pthread_create(&tid, NULL, th_operation, request)) {
+                free_sync();
+                close(req_fifo);
+                unlink(req_fifo_path);
+                free(request);
+                perror("server thread create");
+                return errno;
+            }
+
+            if (pthread_detach(tid)) {
+                free_sync();
+                close(req_fifo);
+                unlink(req_fifo_path);
+                free(request);
+                perror("server thread detach");
+                return errno;
+            }
+        }
+    }
+
+    printf("exited in %ds\n", (int)(time(NULL)-initial));
 
     close(req_fifo);
     unlink(req_fifo_path);
-    return 0;
+    pthread_exit(0);
 }
---
+++ projeto-2/shared/include/sync.h
@@ -0,0 +1,50 @@
+#ifndef SYNC_H_INCLUDED
+#define SYNC_H_INCLUDED
+
+/* MAIN HEADER */
+
+/* INCLUDE HEADERS */
+
+/* SYSTEM CALLS HEADERS */
+#include <sys/types.h>
+#include <unistd.h>
+
+/* C LIBRARY HEADERS */
+#include <semaphore.h>
+
+/* Miscellaneous */
+#include <pthread.h>
+
+/*----------------------------------------------------------------------------*/
+/*                              REQUEST SEMAPHORE                             */
+/*----------------------------------------------------------------------------*/
+
+int init_sync(int oflags);
+
+int free_sync();
+
+int sem_wait_send_request();
+
+int sem_post_send_request();
+
+int sem_wait_receive_request();
+
+int sem_post_receive_request();
+
+/*----------------------------------------------------------------------------*/
+/*                              REPLY SEMAPHORE                               */
+/*----------------------------------------------------------------------------*/
+
+int sem_open_reply(sem_t *sem_reply, pid_t pid, pthread_t tid);
+
+int sem_wait_reply(sem_t *sem_reply);
+
+int sem_post_reply(sem_t *sem_reply);
+
+int sem_close_reply(sem_t *sem_reply);
+
+int sem_unlink_reply(const char *sem_name);
+
+int sem_free_reply(sem_t *sem_reply, pid_t pid, pthread_t tid);
+
+#endif /* end of include guard: SYNC_H_INCLUDED */
